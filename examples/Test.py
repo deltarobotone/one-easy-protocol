afd289270f896e1be12aa185b06792df3ee49f33
buerger-pascal <47589869+buerger-pascal@users.noreply.github.com>
2019-06-24 23:33:19 +0200

    Add files via upload

---
+++ examples/Test.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+
+import OneEasyProtocol as oep
+robot = oep.EasyProtocol(info = True)
+robot.find_robot()
+robot.start()
+
+robot.gripper.open()
+robot.functions.waitFor(2000)
+robot.gripper.close()
+robot.functions.waitFor(2000)
+
+robot.move.ptp(0.0,0.0,85.0,50.0)
+robot.move.ptp(-30.0,0.0,105.0,85.0)
+robot.move.ptp(-30.0,0.0,115.00,20.00)
+robot.move.ptp(-30.0,0.0,105.0,50.0)
+robot.move.ptp(30.0,0.0,105.0,85.0)
+robot.move.ptp(30.0,0.0,115.00,20.00)
+robot.move.ptp(30.0,0.0,105.0,50.0)
+robot.move.ptp(0.0,0.0,105,5.0)
+
+robot.printInfo(False)
+
+color = oep.Color()
+
+robot.light.setColour(color.red,10.0)
+robot.functions.waitFor(1000)
+robot.light.setColour(color.red,50.0)
+robot.functions.waitFor(1000)
+robot.light.setColour(color.red,100.0)
+robot.functions.waitFor(1000)
+
+robot.light.setColour(color.blue)
+robot.functions.waitFor(1000)
+robot.light.setColour(color.green)
+robot.functions.waitFor(1000)
+robot.light.setColour(color.yellow)
+robot.functions.waitFor(1000)
+robot.light.setColour(color.magenta)
+robot.functions.waitFor(1000)
+robot.light.setColour(color.cyan)
+robot.functions.waitFor(1000)
+robot.light.setColour(color.white)
+robot.functions.waitFor(1000)
+
+robot.light.off()
+robot.functions.waitFor(1000)
+
+robot.extmotor.start()
+robot.functions.waitFor(2000)
+robot.extmotor.setSpeed(50.0)
+robot.functions.waitFor(2000)
+robot.extmotor.stop()
+
+robot.stop()
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+from distutils.core import setup
+setup(
+  name = 'one-easy-protocol',
+  packages = ['one-easy-protocol'],
+  version = '0.1',      
+  license='LGPLv3+',
+  description = 'One easy protocol for serial communication between a Device and Delta-Robot One',
+  author = 'Pascal Buerger',
+  author_email = 'deltarobotone@web.de',
+  url = 'https://github.com/deltarobotone',
+  download_url = 'https://github.com/deltarobotone/one-easy-protocol/archive/v_01.tar.gz',
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Topic :: Communications :: ',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
+
